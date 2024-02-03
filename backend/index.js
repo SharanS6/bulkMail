af03631e56ddede9869d78c5d594bf5b49d233d1
SharanS6 <106226147+SharanS6@users.noreply.github.com>
2024-02-03 14:44:35 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const mongoose=require("mongoose")
 
 const app = express()
 app.use(cors({
-    origin:{"https://bulk-mail-api.vercel.app/"},
+    origin:["https://bulk-mail-frontend-iota.vercel.app/"],
     methods:["POST","GET"],
     credentials:true
 }))
