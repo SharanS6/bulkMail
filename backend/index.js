9a8a2ae98f5e5d43823beb4aab10e435d60f42d6
SharanS6 <106226147+SharanS6@users.noreply.github.com>
2024-02-03 15:22:30 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -3,7 +3,7 @@ const cors = require("cors")
 const mongoose=require("mongoose")
 
 const app = express()
-var corsOptions={origin:['https://bulk-mail-frontend-iota.vercel.app']}
+var corsOptions={origin:['https://bulk-mail-frontend-iota.vercel.app'],}
 app.use(cors(corsOptions))
 app.use(express.json())
 
